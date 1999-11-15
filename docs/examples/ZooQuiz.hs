9a75712f2e2df8afc4958c46c89f20f3ab554d10
malcolm <unknown>
1999-11-15 15:22:01 +0000

    ----------------------------------------------------------------------
    Modified Files:
    	docs/examples/ZooQuiz.hs
    
    (Example program was out-of-date, both wrt to the Binary library it
    is supposed to illustrate, and wrt the Haskell 98 standard.  Now
    fixed and tested.)
    ----------------------------------------------------------------------

--- docs/examples/ZooQuiz.hs
+++ docs/examples/ZooQuiz.hs
@@ -5,67 +5,66 @@ import Binary
 import Directory
 import System
 
-data ZooTree = ZT (BinPtr ZooTree) (BinPtr ZooTree) String
+data ZooTree = ZT (Bin ZooTree) (Bin ZooTree) String
                deriving Binary
 
-main =
-  doesFileExist "zoo" >>= \exists->
-  openBin (File "zoo" RW) >>= \zoo ->
-  (if exists then
-      get zoo
-  else
-      put zoo (0,0) >>
-      put zoo (ZT 0 0 "dog") >>= \root->
-      tellBin zoo >>= \eof->
-      putAt zoo 0 (root,eof) >>
-      return (root,eof)
-  ) >>= \(root,eof) ->
-  untilCatch isEOFError 
-    ( \(p, z, end) ->
-     (if p==0 then
-        qanda "Have you thought of an animal"
-          (return ()) 
-          (putAt zoo (unsafeShiftBinPtr sizeofBinPtr 0) end >> 
-           closeBin zoo >>
-           exitWith ExitSuccess)
-      else return ()
-      ) >>
-      getAt zoo z >>= \(ZT y n s) ->
-      if (y == 0) then 
-        qanda ("Is it a "++s++"?")
-          ( getAt zoo 0 >>= \newroot->
-            return (0, newroot, end) )
-          ( putStrLn "What is it then?" >>
-            getLine >>= \t ->
-            putStrLn ("What question has answer yes for a "++s++
-                      " but no for a "++t) >>
-            getLine >>= \q ->
-            putAt zoo end (ZT 0 0 t) >>
-            put zoo (ZT z end q) >>= \qpos->
-            tellBin zoo >>= \newend-> 
-            putAt zoo p qpos >>
-            getAt zoo 0 >>= \newroot->
-            return (0, newroot, newend) )
-      else
-        qanda s
-          (return (unsafeShiftBinPtr 0            z, y, end))
-          (return (unsafeShiftBinPtr sizeofBinPtr z, n, end))
-    )
-    (0 :: BinPtr (BinPtr ZooTree), root, eof)
-    >>= \(_,_,end)->
-    putAt zoo (unsafeShiftBinPtr sizeofBinPtr 0) end >> 
-    closeBin zoo >>
-    exitWith ExitSuccess
+zero = int2BinPtr 0
+
+main = do
+  exists <- doesFileExist "zoo"
+  zoo    <- openBin (File "zoo" RW)
+  (root,eof) <- (if exists then
+                     get zoo
+                 else do
+                     put zoo (zero,zero)
+                     root <- put zoo (ZT zero zero "dog")
+                     eof  <- tellBin zoo
+                     putAt zoo zero (root,eof)
+                     return (root,eof)
+                 )
+  (_,_,end)  <- untilCatch isEOFError 
+                    ( \(p, z, end) -> do
+                      ( if p==zero then
+                          qanda "Have you thought of an animal"
+                            (return ()) 
+                            (finish zoo zero end)
+                         else return ())
+                      (ZT y n s) <- getAt zoo z
+                      ( if (y == zero) then 
+                          qanda ("Is it a "++s++"?")
+                            ( do newroot <- getAt zoo zero
+                                 return (zero, newroot, end) )
+                            ( do putStrLn "What is it then?"
+                                 t <- getLine
+                                 putStrLn ("What question has answer yes for a "
+                                           ++s++" but no for a "++t)
+                                 q <- getLine
+                                 putAt zoo end (ZT zero zero t)
+                                 qpos <- put zoo (ZT z end q)
+                                 newend <- tellBin zoo
+                                 putAt zoo p qpos
+                                 newroot <- getAt zoo zero
+                                 return (zero, newroot, newend) )
+                         else
+                          qanda s
+                            (return (unsafeShiftBinPtr 0            z, y, end))
+                            (return (unsafeShiftBinPtr sizeofBinPtr z, n, end)))
+                    ) (zero {-:: Bin (Bin ZooTree)-}, root, eof)
+  finish zoo zero end
 
 qanda :: String -> (IO a) -> (IO a) -> (IO a)
-qanda q y n =
-  putStrLn q >>
-  getLine >>= \(a:_) ->
+qanda q y n = do
+  putStrLn q
+  (a:_) <- getLine
   case a of
     'y' -> y
     'n' -> n 
     _   -> putStrLn "Start answer y or n." >> qanda q y n
 
+finish zoo zero end = do
+  putAt zoo (unsafeShiftBinPtr sizeofBinPtr zero) end
+  closeBin zoo
+  exitWith ExitSuccess
 
 ------
 untilCatch  :: (IOError->Bool) -> (a->IO a) -> a -> IO a
@@ -73,13 +72,13 @@ untilCatch_ :: (IOError->Bool) ->     IO () ->      IO ()
 
 untilCatch p f a =
   catch (f a >>= \x-> return (True,x))
-        (\e-> if p e then return (False,a) else fail e)
+        (\e-> if p e then return (False,a) else ioError e)
   >>= \(ok,a')->
   if ok then untilCatch p f a' else return a'
 
 untilCatch_ p f =
   catch (f >> return True)
-        (\e-> if p e then return False else fail e)
+        (\e-> if p e then return False else ioError e)
   >>= \ok->
   if ok then untilCatch_ p f else return ()
 
