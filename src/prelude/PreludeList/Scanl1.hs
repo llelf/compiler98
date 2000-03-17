02c9c8f1470acf4b73dd81e7160202027e24363b
malcolm <unknown>
2000-03-17 15:17:07 +0000

    ----------------------------------------------------------------------
    Modified Files:
    	script/fixghc src/compiler98/Makefile src/greencard/Makefile
    	src/hmake/Makefile
    (Yet more (minor) changes for ghc-4.06)
    
    	src/prelude/PreludeList/Scanl1.hs src/prelude/PreludeList/Scanr1.hs
    (SPJ proposes to change the Haskell'98 definition of these functions.)
    
    Removed Files:
    	src/compiler98/NhcFloats_.hs
    (This is a regeneratible file which doesn't belong in CVS.)
    
    ----------------------------------------------------------------------

--- src/prelude/PreludeList/Scanl1.hs
+++ src/prelude/PreludeList/Scanl1.hs
@@ -2,4 +2,5 @@ module Prelude where
 
 scanl1	   	 :: (a -> a -> a) -> [a] -> [a]
 scanl1 f (x:xs)  = scanl f x xs 
-scanl1 f []      = error "PreludeList.scanl1: empty list"
+scanl1 f []      = []
+--scanl1 f []    = error "PreludeList.scanl1: empty list"
--- src/prelude/PreludeList/Scanr1.hs
+++ src/prelude/PreludeList/Scanr1.hs
@@ -4,4 +4,5 @@ scanr1	  	:: (a -> a -> a) -> [a] -> [a]
 scanr1 f [x] 	= [x] 
 scanr1 f (x:xs) = f x q : qs
 		  where qs@(q:_) = scanr1 f xs
-scanr1 _ [] 	= error "PreludeList.scanr1: empty list"
+scanr1 _ [] 	= []
+--scanr1 _ [] 	= error "PreludeList.scanr1: empty list"
