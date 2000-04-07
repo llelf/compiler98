6033270957cc4da8e706fa79298d2fa372a6b20f
malcolm <unknown>
2000-04-07 14:49:39 +0000

    ----------------------------------------------------------------------
    Added Files:
    	src/compiler98/ReportImports.hs
    
    (Added -report-imports flag to compiler.)
    ----------------------------------------------------------------------

---
+++ src/compiler98/ReportImports.hs
@@ -0,0 +1,52 @@
+module ReportImports where
+
+import List(nub,groupBy,intersperse,sortBy)
+import PackedString(unpackPS)
+import TokenId
+import Info
+import IntState
+import Tree234
+
+reportImports :: String -> IntState -> [String]
+reportImports m =
+  map ("import "++) .
+  nub .
+  map moduleName .
+  filter ((m/=).moduleName) .
+  concatMap (possibleTid.snd) .
+  treeMapList (:) .
+  getSymbolTable
+
+reportFnImports :: String -> IntState -> [String]
+reportFnImports m =
+  map (\xs-> "import "++ fst (head xs) ++ "\t(" ++
+             concat (intersperse ", " (map snd xs)) ++ ")") .
+  groupBy (\(x,_) (y,_)-> x==y) .
+  sortBy  (\(x,_) (y,_)-> compare x y) .
+  map (\t-> (moduleName t, varName t)) .
+  filter (("Prelude"/=).moduleName) .
+  filter ((m/=).moduleName) .
+  concatMap (possibleTid.snd) .
+  treeMapList (:) .
+  getSymbolTable
+
+moduleName :: TokenId -> String
+moduleName = reverse . unpackPS . TokenId.extractM
+
+varName :: TokenId -> String
+varName = reverse . unpackPS . TokenId.extractV
+
+possibleTid (InfoClear)                                  = []
+possibleTid (InfoUsed   unique uses)                     = []
+possibleTid (InfoUsedClass unique uses insts)            = []
+possibleTid (InfoData   unique tid exp nt dk)            = [tid]
+possibleTid (InfoClass  unique tid exp nt ms ds insts)   = [tid]
+possibleTid (InfoVar    unique tid fix exp nt annot)     = [tid]
+possibleTid (InfoConstr unique tid fix nt fields iType)  = []
+possibleTid (InfoField  unique tid icon_offs iData iSel) = []
+possibleTid (InfoMethod unique tid fix nt annot iClass)  = []
+possibleTid (InfoIMethod unique tid nt annot iMethod)    = []
+possibleTid (InfoDMethod unique tid nt annot iClass)     = []
+possibleTid (InfoInstance unique  nt iClass)             = []
+possibleTid (InfoName _ tid _ _)			 = [tid]
+
