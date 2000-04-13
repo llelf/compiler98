25f82d6fb6e171dad0774819526675be38975f92
malcolm <unknown>
2000-04-13 11:32:39 +0000

    ----------------------------------------------------------------------
    Modified Files:
    	include/GreenCard.gc src/prelude/CPUTime/CPUTime.hs
    	src/prelude/CPUTime/Makefile
    	src/prelude/GreenCard/GreenCard.gc src/prelude/Time/Makefile
    	src/runtime/Mk/Makefile
    Added Files:
    	src/prelude/Time/DCalendarTime.gc src/prelude/Time/DDay.gc
    	src/prelude/Time/DMonth.gc src/prelude/Time/ToCalendarTime.gc
    	src/prelude/Time/ToClockTime.gc src/prelude/Time/ToUTCTime.gc
    Removed Files:
    	include/PrelRatio.hi src/prelude/Time/DCalendarTime.hs
    	src/prelude/Time/DDay.hs src/prelude/Time/DMonth.hs
    	src/prelude/Time/ToCalendarTime.hs
    	src/prelude/Time/ToClockTime.hs src/prelude/Time/ToUTCTime.hs
    
    (New implementations of libraries CPUTime and Time.  Removed PrelRatio
     (replaced by just Ratio) and added `%dis integer' to GreenCard.)
    ----------------------------------------------------------------------

--- src/prelude/CPUTime/CPUTime.hs
+++ src/prelude/CPUTime/CPUTime.hs
@@ -1,10 +1,11 @@
-module CPUTime where
+module CPUTime
+  ( getCPUTime
+  , cpuTimePrecision
+  ) where
 
-import Warning
+--getCPUTime       :: IO Integer
+--cpuTimePrecision :: Integer
 
-getCPUTime       :: IO Integer
-cpuTimePrecision :: Integer
-
-getCPUTime        = warning "CPUTime.getCPUTime: not implemented" (return 0)
-cpuTimePrecision  = warning "CPUTime.cpuTimePrecision: not implemented" 0
+import GetCPUTime
+import CpuTimePrecision
 
--- src/prelude/Time/ToClockTime.hs
+++
@@ -1,9 +0,0 @@
-module Time where
-
-import Warning
-import DClockTime
-import DCalendarTime
-
-toClockTime :: CalendarTime -> ClockTime
-toClockTime ct = warning "Time.toClockTime: not implemented" (CT 0)
-
