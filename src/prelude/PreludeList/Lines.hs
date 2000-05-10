c0ecf530ba3929f577d3599e3cabc468ec5efc07
malcolm <unknown>
2000-05-10 11:46:24 +0000

    Wolfram Kahl suggested on the Haskell mailing list to recode "lines"
    as a foldr - to improve its readability and efficiency.

--- src/prelude/PreludeList/Lines.hs
+++ src/prelude/PreludeList/Lines.hs
@@ -1,8 +1,31 @@
 module Prelude where
 
 lines	   	:: String -> [String]
-lines ""	= []
-lines s		= let (l,s') = break (== '\n') s 
-		  in l : case s' of
-			      []      -> []
-			      (_:s'') -> lines s''
+
+-- specification in the Report:
+--
+-- lines ""	= []
+-- lines s	= let (l,s') = break (== '\n') s 
+--		  in l : case s' of
+--			      []      -> []
+--			      (_:s'') -> lines s''
+
+
+-- more efficient:
+--
+-- lines = foldr f []
+--   where
+--     f '\n' xss = "" : xss
+--     f x [] = [[x]]
+--     f x (ys:yss) = (x:ys) : yss
+
+
+
+-- more efficient still:
+--
+lines [] = []
+lines (x:xs) = f x (lines xs)
+  where
+     f '\n' xss = "" : xss
+     f x [] = [[x]]
+     f x (ys:yss) = (x:ys) : yss
