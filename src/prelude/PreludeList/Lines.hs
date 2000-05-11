57e53bf29a499be3b624c3dbafdc985d01fa651b
malcolm <unknown>
2000-05-11 10:42:29 +0000

    It appears that Wolfram Kahl's more efficient "lines" is also less lazy,
    so I'm reverting to the standard definition for now.

--- src/prelude/PreludeList/Lines.hs
+++ src/prelude/PreludeList/Lines.hs
@@ -4,14 +4,14 @@ lines	   	:: String -> [String]
 
 -- specification in the Report:
 --
--- lines ""	= []
--- lines s	= let (l,s') = break (== '\n') s 
---		  in l : case s' of
---			      []      -> []
---			      (_:s'') -> lines s''
+lines ""	= []
+lines s	= let (l,s') = break (== '\n') s 
+	  in l : case s' of
+		      []      -> []
+		      (_:s'') -> lines s''
 
 
--- more efficient:
+-- more efficient but less lazy:
 --
 -- lines = foldr f []
 --   where
@@ -23,9 +23,20 @@ lines	   	:: String -> [String]
 
 -- more efficient still:
 --
-lines [] = []
-lines (x:xs) = f x (lines xs)
-  where
-     f '\n' xss = "" : xss
-     f x [] = [[x]]
-     f x (ys:yss) = (x:ys) : yss
+-- lines [] = []
+-- lines (x:xs) = f x (lines xs)
+--   where
+--      f '\n' xss = "" : xss
+--      f x [] = [[x]]
+--      f x (ys:yss) = (x:ys) : yss
+
+
+
+-- even more efficient:
+--
+-- lines s = lines' s id
+--   where
+--   lines' []       acc = [acc []]
+--   lines' ('\n':s) acc = acc [] : lines' s id
+--   lines' (c:s)    acc = lines' s (acc . (c:))
+
