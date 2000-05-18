5226939dd752ed67606fce5d483f769c3e4bc68b
malcolm <unknown>
2000-05-18 16:31:23 +0000

    Changed banner slightly, added :type command (but seems rather dodgy!)
    and added stubs for evaluating within the scope of a module.

--- src/interpreter/hi.hs
+++ src/interpreter/hi.hs
@@ -17,6 +17,7 @@ main = do
   let opts = options ++ defaultOptions defaultCompiler
   putStrLn banner
   putStrLn (replicate 43 ' '++ "... Using compiler "++show defaultCompiler++" ...\n")
+  putStrLn ("Type :? for help")
   hSetBuffering stdout NoBuffering
   load opts defaultCompiler "Prelude"
   toplevel opts defaultCompiler ["Prelude"]
@@ -57,6 +58,37 @@ evaluate expr options compiler modules = do
   hClose f
   compile options compiler tmpfile (run tmpfile [])
 
+{-
+-- scopedEvaluate allows you to play with _unexported_ functions from the
+-- current module.  This currently requires the restriction that "main" is
+-- not otherwise defined in this module.
+scopedEvaluate expr options compiler modules = do
+  -- Ok, this isn't fully implemented yet, but the basic scenario is
+  -- to copy the module to /tmp/Main.hs, removing a "module ... where"
+  -- header if it exists (could replace with "module Main where", but
+  -- that is unnecessary).  Add a "main = ..." definition to the end
+  -- of the module, compile and run.  Additionally, it would be nice to
+  -- search for an existing "main" definition and rename it to "_main",
+  -- but that is (a) harder, and (b) potentially time-consuming.
+  let tmpfile = "/tmp/Main"
+  system ("echo module Main where >"++tmpfile++".hs")
+  -- system ("cat " ...)
+  hPutStr f (
+    "module Main where\n\n" ++
+    concatMap (\m-> "import "++m++"\n") modules ++
+    "\n" ++ nonstdCoerce compiler ++
+    "\n" ++ nonstdShow compiler ++
+    "\nmain = let expr  = (" ++ expr ++ ")" ++
+    "\n           shown = show expr" ++
+    "\n       in case shown of" ++
+    "\n           ('<':'<':'I':'O':_) -> coerce expr" ++
+    "\n           _                   -> putStrLn shown" ++
+    "\n")
+  hClose f
+  compile options compiler tmpfile (run tmpfile [])
+  where
+-}
+
 compile options compiler file continue = do
   putStr "[Compiling..."
 --debug ("hmake -"++show compiler++" -I. "++unwords options++" "++file++" >/dev/null")
@@ -90,7 +122,7 @@ commands ws options compiler modules =
              toplevel options compiler mods	-- explicit return with new module list
        )
   command "reload"
-       (do makeclean ".o" modules
+       (do -- makeclean ".o" modules	-- NO, let hmake rebuild from changes
            loadAll options compiler modules)
   command "edit"
       (if null target then do
@@ -193,14 +225,25 @@ makeclean ext modules = mapM_ (clean ext) modules
         catch (removeFile file) print
       else done
 
-fromOpt prefix opt =
-  if prefix `isPrefixOf` opt then Just (drop (length prefix) opt) else Nothing
+--fromOpt prefix opt =
+--  if prefix `isPrefixOf` opt then Just (drop (length prefix) opt) else Nothing
 
+{-
+scope file mod = do
+  system ("sed -e '/^module .* where.*/d' "++file++" >/tmp/"++file)
+  return ("/tmp/"++file)
+-}
+
+#if defined(__HBC__)
+banner = "hi - hmake interactive                (Version: "++hmakeversion++")"
+help   = "hi - help command does not work in hbc"
+
+#else
 banner = "\ 
-\__   __           ____  __             _____________________________________
-||   ||  ______  ____|| || _  ____     hmake interactive:
-||___|| || || || ||  || ||/  ||__||       Copyright (c) 1st-2nd May 2000
-||---|| || || || ||__|  ||\\_ ||__         http://www.cs.york.ac.uk/fp/hmake/
+\__   __                 __             _____________________________________
+||   ||  ______    ___  || _  ____     hmake interactive (hi):
+||___|| || || ||  ___|| ||/  ||__||       Copyright (c) May 2000
+||---|| || || || ||__|| ||\\_ ||__         http://www.cs.york.ac.uk/fp/hmake/
 ||   ||                                Report bugs to: malcolm@cs.york.ac.uk
 ||   || Version: "++hmakeVersion++"    -------------------------------------"
 
@@ -208,18 +251,21 @@ banner = "\
 help = "\ 
 \Commands (can be abbreviated to first letter):
   <expr>		evaluate expression
+  :type <expr>	 	show type of expression [nhc98 only]
   :quit			quit
   :Quit			quit
-  :load mod [mod...]	load modules
+  :load mod [mod...]	load modules (note, not filenames)
   :load 		clear all modules
-  :also mod [mod...]	load additional modules
   :reload 		repeat last load command
+  :also mod [mod...]	load additional modules (note, not filenames)
+  :module mod		set module scope for evaluating expressions
   :edit file	 	edit filename
-  :edit 	 	edit last module
+  :edit 	 	edit current module
   :cd dir		change directory
   :cd 			show current directory
   :dir 			list current directory
-  :hc compiler		set compiler to use
-  :set options		set hmake options
-  :unset options	remove hmake options
+  :hc compiler		set Haskell compiler to use
+  :set options		set hmake/compiler options
+  :unset options	remove hmake/compiler options
   :!command 		shell escape"
+#endif
