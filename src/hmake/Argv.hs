465a40fa68359de917c166a8eb2f373c6425d1e2
malcolm <unknown>
2000-06-14 23:11:37 +0000

    Added new options -clean and -realclean to remove relevant .o and .hi files.

--- src/hmake/Argv.hs
+++ src/hmake/Argv.hs
@@ -11,6 +11,26 @@ instance Show Goal where
 stripGoal (Object name)  = name
 stripGoal (Program name) = name
 
+-- The following is the start of an attempt to replace the 12-tuple
+-- returned by "decode" with something more meaningful.  Apart from
+-- anything else, nhc98 doesn't do 13-tuples, so this is needed if
+-- you are to make any extensions to the type.
+--
+--data DecodedArgs =
+--     Decoded
+--	[String]		-- cpp options beginning -Z
+--	[String]		-- cpp options beginning -D
+--	Bool			-- does compiler have a -d option?
+--	(String->String)	-- conditional debuggging
+--	([String]->String->String)  -- conditional on option being unset
+--	([String]->String->String)  -- conditional on option being set
+--	(String->Bool)		-- is option set?
+--	String			-- goal Directory for .o files
+--	[String]		-- list of .hi files to ignore
+--	[String]		-- list of paths to compilable sources
+--	[String]		-- list of paths to prelude .hi files
+--	[String]		-- list of specified target modules
+
 decode progArgs = 
    (zdefs, defs, dflag, ifdebug, ifnotopt, ifopt, isopt, goalDir, ignoreHi,
     pathSrc, pathPrel, modules)
@@ -31,7 +51,7 @@ decode progArgs =
 
   isopt opt = opt `elem` flags
 
-  ifopt opts s = if or (map isopt opts) then s else ""
+  ifopt opts s = if any isopt opts then s else ""
 
   ifnotopt opts s =
     if not (or (map isopt opts)) then s else ""
--- src/hmake/Output.hs
+++ src/hmake/Output.hs
@@ -1,4 +1,4 @@
-module Output(qCompile,qLink) where
+module Output(qCompile,qLink,qCleano,qCleanhi) where
 import ListUtil -- (lconcatMap)
 import FileName
 import ListUtil
@@ -8,41 +8,49 @@ import Argv  -- Goal
 doEcho True cmd = "echo \"" ++ cmd ++ "\"\n" ++ cmd ++ "\n"
 doEcho False cmd = cmd ++ "\n"
 
+oFile goalDir unix path mod =
+    if null goalDir then fixFile unix path    mod "o"
+                    else fixFile unix goalDir mod "o"
+hiFile unix path mod  =  fixFile unix path    mod "hi"
+
+qCleano  echo goalDir unix (_,(p,m,source,cpp)) =
+    doEcho echo ("rm -f " ++ oFile goalDir unix p m)
+qCleanhi echo unix (_,(p,m,source,cpp)) =
+    doEcho echo ("rm -f " ++ hiFile unix p m)
+
 qCompile echo goalDir dflag defines unix (dep,(p,m,source,cpp)) =
   if null dep
   then doEcho echo compilecmd
   else test m dep compilecmd
  where
---goal = if goalDir=="" then "." else goalDir
-  ofile = if null goalDir then fixFile unix p       m "o"
-                          else fixFile unix goalDir m "o"
-  compilecmd = compile source p m
-  cppcmd = if cpp then "-cpp"++concat (map doD defines)++" " else ""
-  doD s = " -D"++s
-
-  compile source path mod =
+  ofile = oFile goalDir unix p m
+  compilecmd =
     compiler ++ "-c " ++ cppcmd ++
     (if dflag then "-d "++goalDir else "-o "++ofile) ++
     " " ++ source
 
+  compiler = if unix then "${HC} ${HFLAGS} " else "nhc98 "
+  cppcmd = if cpp then "-cpp"++concat (map doD defines)++" " else ""
+  doD s = " -D"++s
+
+--goal = if goalDir=="" then "." else goalDir
 --compile source = compiler++" -c "++source
 --compile source path mod =
 --  compiler ++ " -c " ++ cppcmd ++ source {-fixFile unix path mod "hs"-}
 --  ++ (if dflag || (path==goal) || (path/=".") then ""
 --      else " -o " ++ fixFile unix goalDir mod "o")
 
-  compiler = if unix then "${HC} ${HFLAGS} " else "nhc98 "
 
   test = if unix
          then (\m dep comp ->
                "if [ `$OLDER " ++ ofile
-		++ lconcatMap (\(d,p) -> ' ':fixFile unix p d "hi") dep
+		++ lconcatMap (\(d,p) -> ' ':hiFile unix p d) dep
 	        ++"` = 1 ]\nthen\n"
 	        ++ doEcho echo compilecmd
  	        ++ "fi\n")
          else (\m dep comp ->
-               "older " ++ fixFile unix goalDir m "o" 
-                        ++ lconcatMap (\(d,p) -> ' ':fixFile unix p d "hi") dep
+               "older " ++ ofile
+                        ++ lconcatMap (\(d,p) -> ' ':hiFile unix p d) dep
                 ++ "\nset Nhc$ReturnCode <Sys$ReturnCode>\n"
 --                ++ (if echo then "IF <Nhc$ReturnCode> THEN echo " ++ compilecmd else [])
                 ++ "IF <Nhc$ReturnCode> THEN " ++ compilecmd)
