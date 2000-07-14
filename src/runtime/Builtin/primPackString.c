69b36dc75e8e21a68441ab98a477e6a8ce15cdba
malcolm <unknown>
2000-07-14 10:47:16 +0000

    Revert from null termination of PackedString - it introduces problems,
    and wasn't necessary anyhow.

--- src/runtime/Builtin/primPackString.c
+++ src/runtime/Builtin/primPackString.c
@@ -17,7 +17,7 @@ C_HEADER(primPackString)
   IND_REMOVE(res);
   length = GET_INT_VALUE(res);
 
-  size = (length +1 + sizeof(Node) -1) / sizeof(Node);
+  size = (length + sizeof(Node) -1) / sizeof(Node);
   extra = size * sizeof(Node) - length;
 
   res = C_ALLOC(1+EXTRA+size);
@@ -34,7 +34,6 @@ C_HEADER(primPackString)
     *dstptr++ = GET_CHAR_VALUE(chr);
     src = GET_POINTER_ARG1(src,2);
   }
-  *dstptr = 0;	/* MW, 13.07.2000: ensure null termination? */
   C_RETURN(res);
 }	
 
