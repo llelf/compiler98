3bc77526621d97c2d732748e556b04f99bf63259
malcolm <unknown>
2000-07-19 16:34:15 +0000

    Change the tracing compilation of _EqInteger.hs - this auxiliary
    function is used by the compiler to implement pattern-matches on
    numeric literals.  Thus, it must have a basic type, even when tracing.

--- src/prelude/Prelude/_EqInteger.hs
+++ src/prelude/Prelude/_EqInteger.hs
@@ -2,12 +2,16 @@ module Prelude where
 
 import PrimIntegerEq
 
+-- used when compiling case-expressions if pattern is known to be Integer
+foreign import "primIntegerEqC" _eqInteger :: Integer -> Integer -> Bool
 
+
+
+#if 0
 -- used when compiling case-expressions if pattern is known to be Integer
 _eqInteger a b  = primIntegerEq a b 
 
 
-#if 0
 -- used when compiling case-expressions if pattern is known to be Integer
 _eqInteger a b  = _prim _tprim_EqInteger a b 
 
