2cae63bda0c7af2f91defd91dda111474f70ae87
malcolm <unknown>
2000-07-19 16:38:11 +0000

    Add an FFI-able version of unpackPS.

--- src/runtime/Builtin/unpackPS.c
+++ src/runtime/Builtin/unpackPS.c
@@ -31,3 +31,34 @@ C_HEADER(unpackPS)
 
   C_RETURN(nodeptr);
 }	
+
+/* unpackPSC ::  E PackedString -> [Char] */
+NodePtr unpackPSC (NodePtr e)
+{
+  int length;
+  NodePtr nodeptr,prevptr;
+  Coninfo cinfo;
+  char *srcptr;
+
+  /* outer constructor is required only to fool FFI not to take string apart. */
+  nodeptr = GET_POINTER_ARG1(e,1);
+  IND_REMOVE(nodeptr);
+  cinfo =  GET_CONINFO(nodeptr);
+  length = CONINFO_LARGESIZES(cinfo) * sizeof(Node) - CONINFO_LARGEEXTRA(cinfo);
+
+  C_CHECK(length * sizeCons);
+
+  nodeptr = C_GETARG1(1);
+  IND_REMOVE(nodeptr);
+  srcptr = (char*)&nodeptr[1+EXTRA];
+
+  prevptr = (NodePtr)&nodeptr;
+  while(length--) {
+    int c = *srcptr++;
+    *prevptr = (Node)mkCons(mkChar(c),0);
+    prevptr = &((NodePtr)(*prevptr))[EXTRA+2];
+  }
+  *prevptr = (Node)mkNil();
+
+  return (nodeptr);
+}
