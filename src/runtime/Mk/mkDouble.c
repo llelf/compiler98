57d7cf9743d953f3c669a5a71d8896eb4690575d
malcolm <unknown>
2000-08-22 11:31:36 +0000

    Solved Float->Double promotion problem in FFI by #including the correct
    header file here.

--- src/runtime/Mk/mkDouble.c
+++ src/runtime/Mk/mkDouble.c
@@ -1,9 +1,10 @@
 #include "mk.h"
+#include "mutlib.h"
 
 NodePtr mkDouble(double d)
 {
   NodePtr n;
-  n = C_ALLOC(1+EXTRA+2);
+  n = C_ALLOC(SIZE_DOUBLE);
   INIT_PROFINFO(n,&dummyProfInfo)
   mk_double(n,d);
   return n;
--- src/runtime/Mk/mkFloat.c
+++ src/runtime/Mk/mkFloat.c
@@ -1,9 +1,10 @@
 #include "mk.h"
+#include "mutlib.h"
 
 NodePtr mkFloat(float f)
 {
   NodePtr n;
-  n = C_ALLOC(1+EXTRA+1);
+  n = C_ALLOC(SIZE_FLOAT);
   INIT_PROFINFO(n,&dummyProfInfo)
   mk_float(n,f);
   return n;
