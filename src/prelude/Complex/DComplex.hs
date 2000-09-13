fadaaf71720fd40e9e8dc61b1ccd6f8f121cd587
malcolm <unknown>
2000-09-13 13:05:00 +0000

    Add library Complex for tracing, and fix incorrect instance of Read.

--- src/prelude/Complex/DComplex.hs
+++ src/prelude/Complex/DComplex.hs
@@ -2,4 +2,8 @@ module Complex where
 
 infix 6 :+
 
+#if !defined(TRACING)
 data  (RealFloat a) => Complex a = !a :+ !a
+#else
+data  (RealFloat a) => Complex a =  a :+  a
+#endif
--- src/prelude/Complex/Read_Complex.hs
+++ src/prelude/Complex/Read_Complex.hs
@@ -3,6 +3,7 @@ module Complex(Read(..)) where
 import DComplex
 
 instance (Read a, RealFloat a) => Read (Complex a) where
-  readsPrec p =	 readParen (p >= 6) ( \r -> [(x :+ y,v) | (x,v) <- readsPrec 6 r,
-							  ("Left",u) <- lex v,
-							  (y,w) <- readsPrec 6 u])
+  readsPrec p =	 readParen (p >= 6) (\r -> [(x :+ y,v) | (x,v) <- readsPrec 6 r,
+							 (":+",u) <- lex v,
+							 (y,w) <- readsPrec 6 u])
+
