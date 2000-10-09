538f943355140e5e087416aa88a8053f8b18b8bc
malcolm <unknown>
2000-10-09 10:08:13 +0000

    Re-instate our earlier default implementation of atan2.  We don't
    implement isNegativeZero, so the atan2 definition from the Report is
    useless to us.

--- src/prelude/Prelude/CRealFloat.hs
+++ src/prelude/Prelude/CRealFloat.hs
@@ -14,20 +14,25 @@ class  (RealFrac a, Floating a) => RealFloat a  where
 			:: a -> Bool
     atan2               :: a -> a -> a
 
-    exponent x		= case decodeFloat x of (m,n) -> if m == 0 then 0 else n + floatDigits x
+    exponent x		= case decodeFloat x of
+                              (m,n) -> if m == 0 then 0 else n + floatDigits x
+    significand x	= case decodeFloat x of
+                              (m,_) -> encodeFloat m (negate (floatDigits x))
+    scaleFloat k x	= case decodeFloat x of
+                              (m,n) -> encodeFloat m (n+k)
 
-    significand x	= case decodeFloat x of (m,_) -> encodeFloat m (negate (floatDigits x))
+    atan2 y x = case (signum y, signum x) of
+                    ( 0, 1) -> 0
+                    ( 1, 0) -> pi/2
+                    ( 0,-1) -> negate pi
+                    (-1, 0) -> negate pi/2
+                    ( _, 1) -> atan (y/x)
+                    ( _,-1) -> atan (y/x) + pi
+                    ( 0, 0) -> error "Prelude.atan2: atan2 of origin"
 
-    scaleFloat k x	= case decodeFloat x of (m,n) -> encodeFloat m (n+k)
-
---  atan2 y x = case (signum y, signum x) of
---                  ( 0, 1) -> 0
---                  ( 1, 0) -> pi/2
---                  ( 0,-1) -> negate pi
---                  (-1, 0) -> negate pi/2
---                  ( _, 1) -> atan (y/x)
---                  ( _,-1) -> atan (y/x) + pi
---                  ( 0, 0) -> error "Prelude.atan2: atan2 of origin"
+{- The following is the specification of atan2 from the Haskell Report.
+-- However, because we don't implement isNegativeZero, this will always
+-- fail.  Hence, the alternative implementation given above.
     atan2 y x
       | x>0          = atan (y/x)
       | x==0 && y>0  = pi / 2
@@ -39,4 +44,4 @@ class  (RealFrac a, Floating a) => RealFloat a  where
                      = pi	-- must be after the previous test on zero y
       | x==0 && y==0 = y	-- must be after the other double zero tests
       | otherwise    = x + y	-- x or y is a NaN, return a NaN (via +)
-
+-}
