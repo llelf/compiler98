d12bb4c590becff0c65a3ce93e03fa7f792ef76d
malcolm <unknown>
2001-01-02 17:52:05 +0000

    More filling-in of Int8/16/32/64 types implementation.

--- src/prelude/FFI/Int.hs
+++ src/prelude/FFI/Int.hs
@@ -8,64 +8,93 @@ module FFI
   ) where
 
 import FFIBuiltin (Int8, Int16, Int32, Int64)
+import Numeric    (readSigned,readDec,showSigned,showIntBase)
 
--- Int8
-foreign import primEqInt8     :: Int8 -> Int8 -> Int8
-foreign import primLeInt8     :: Int8 -> Int8 -> Bool
-foreign import primAddInt8    :: Int8 -> Int8 -> Int8
-foreign import primMulInt8    :: Int8 -> Int8 -> Int8
-foreign import primAbsInt8    :: Int8 ->         Int8
-foreign import primSignumInt8 :: Int8 ->         Int8
-foreign import primQuotInt8   :: Int8 -> Int8 -> Int8
-foreign import primRemInt8    :: Int8 -> Int8 -> Int8
 
-foreign cast   primToEnumInt8 :: Int  ->         Int8
-foreign cast primFromEnumInt8 :: Int8 ->         Int
+#define INT_TYPE(T,LB,UB)	\
+FOREIGNS(T)			;\
+INSTANCE_EQ(T)			;\
+INSTANCE_ORD(T)			;\
+INSTANCE_NUM(T)			;\
+INSTANCE_BOUNDED(T,LB,UB)	;\
+INSTANCE_REAL(T)		;\
+INSTANCE_INTEGRAL(T)		;\
+INSTANCE_IX(T)			;\
+INSTANCE_ENUM(T)		;\
+INSTANCE_READ(T)		;\
+INSTANCE_SHOW(T)
 
-instance Eq Int8 where
-  (==) = primEqInt8
+#define FOREIGNS(T)	\
+foreign import primEq##T		:: T -> T  -> T		;\
+foreign import primLe##T		:: T -> T  -> Bool	;\
+foreign import primAdd##T		:: T -> T  -> T		;\
+foreign import primMul##T		:: T -> T  -> T		;\
+foreign import primAbs##T		:: T       -> T		;\
+foreign import primSignum##T		:: T       -> T		;\
+foreign import primQuot##T		:: T -> T  -> T		;\
+foreign import primRem##T		:: T -> T  -> T		;\
+foreign cast   primToEnum##T		:: Int     -> T		;\
+foreign cast primFromEnum##T		:: T       -> Int	;\
+foreign import prim##T##FromInteger	:: Integer -> T		;\
+foreign import prim##T##ToInteger	:: T       -> Integer
 
-instance Ord Int8 where
-  (<=) = primLeInt8
 
-instance Num Int8 where
-  (+) = primAddInt8
-  (*) = primMulInt8
-  abs = primAbsInt8
-  signum = primSignumInt8
-  fromInteger = toEnum . fromInteger
+#define INSTANCE_EQ(T)	\
+instance Eq T where 	{\
+  (==) = primEq##T	}
 
-instance Bounded Int8 where
-  minBound = toEnum (-128)
-  maxBound = toEnum 127
+#define INSTANCE_ORD(T)	\
+instance Ord T where	{\
+  (<=) = primLe##T	}
 
-instance Real Int8 where
-  toRational i = fromEnum i % 1
+#define INSTANCE_NUM(T)			\
+instance Num T where			{\
+  (+) = primAdd##T			;\
+  (*) = primMul##T			;\
+  abs = primAbs##T			;\
+  signum = primSignum##T		;\
+  fromInteger = prim##T##FromInteger	}
 
-instance Integral Int8 where
-  quot = primQuotInt8
-  rem  = primRemInt8
+#define INSTANCE_BOUNDED(T,LB,UB)	\
+instance Bounded T where		{\
+  minBound = fromInteger LB		;\
+  maxBound = fromInteger UB		}
 
-instance Ix Int8 where
-  range (m,n) = [m..n]
-  index b@(m,n) i
-        | inRange b i = i - m
-        | True         = error "Ix.Int.index: Index out of range."
-  inRange (m,n) i    = m <= i && i <= n
+#define INSTANCE_REAL(T)		\
+instance Real T where			{\
+  toRational i = toInteger i % 1	}
 
-instance Enum Int8 where
-  toEnum = primToEnumInt8
-  fromEnum = primFromEnumInt8
-#if !defined(TRACING)
-  enumFrom =  numericEnumFrom
-  enumFromThen = numericEnumFromThen
-#else
-  enumFrom n = n : enumFrom (n+1)
-  enumFromThen m n = m : enumFromThen n (2*n-m)
-#endif
+#define INSTANCE_INTEGRAL(T)		\
+instance Integral T where		{\
+  quot = primQuot##T			;\
+  rem  = primRem##T			;\
+  toInteger = prim##T##ToInteger	}
 
-instance Read Int8 where
-  readsPrec p = map (\(a,b)->(toEnum a,b)) . readSigned readDec
+#define INSTANCE_IX(T)			\
+instance Ix T where			{\
+  range (m,n) = [m..n]			;\
+  index b@(m,n) i			;\
+        | inRange b i = fromIntegral (i-m)			;\
+        | True         = error "Ix.index: Index out of range."	;\
+  inRange (m,n) i    = m <= i && i <= n	}
 
-instance Show Int8 where
-  showsPrec p = showsPrec p . fromEnum
+#define INSTANCE_ENUM(T)		\
+instance Enum T where			{\
+  toEnum = primToEnum##T		;\
+  fromEnum = primFromEnum##T		;\
+  enumFrom n = n : enumFrom (n+1)	;\
+  enumFromThen m n = m : enumFromThen n (2*n-m)		}
+
+#define INSTANCE_READ(T)		\
+instance Read T where			{\
+  readsPrec p = readSigned readDec	}
+
+#define INSTANCE_SHOW(T)			\
+instance Show T where				{\
+  showsPrec p = showSigned (showIntBase 16) p	}
+
+
+INT_TYPE(Int8,(-128),127)
+INT_TYPE(Int16,(-32768),32767)
+INT_TYPE(Int32,(-2147483648),2147483647)
+INT_TYPE(Int64,(-18446744073709551616),18446744073709551615)
