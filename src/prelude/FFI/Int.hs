a3e69eabcb1cd80c6b1ac39dcde410f2807de348
malcolm <unknown>
2001-01-02 10:15:38 +0000

    Begin to add full support for Int8/16/64.

--- src/prelude/FFI/Int.hs
+++ src/prelude/FFI/Int.hs
@@ -9,5 +9,63 @@ module FFI
 
 import FFIBuiltin (Int8, Int16, Int32, Int64)
 
-instance Num Int8 where
+-- Int8
+foreign import primEqInt8     :: Int8 -> Int8 -> Int8
+foreign import primLeInt8     :: Int8 -> Int8 -> Bool
+foreign import primAddInt8    :: Int8 -> Int8 -> Int8
+foreign import primMulInt8    :: Int8 -> Int8 -> Int8
+foreign import primAbsInt8    :: Int8 ->         Int8
+foreign import primSignumInt8 :: Int8 ->         Int8
+foreign import primQuotInt8   :: Int8 -> Int8 -> Int8
+foreign import primRemInt8    :: Int8 -> Int8 -> Int8
 
+foreign cast   primToEnumInt8 :: Int  ->         Int8
+foreign cast primFromEnumInt8 :: Int8 ->         Int
+
+instance Eq Int8 where
+  (==) = primEqInt8
+
+instance Ord Int8 where
+  (<=) = primLeInt8
+
+instance Num Int8 where
+  (+) = primAddInt8
+  (*) = primMulInt8
+  abs = primAbsInt8
+  signum = primSignumInt8
+  fromInteger = toEnum . fromInteger
+
+instance Bounded Int8 where
+  minBound = toEnum (-128)
+  maxBound = toEnum 127
+
+instance Real Int8 where
+  toRational i = fromEnum i % 1
+
+instance Integral Int8 where
+  quot = primQuotInt8
+  rem  = primRemInt8
+
+instance Ix Int8 where
+  range (m,n) = [m..n]
+  index b@(m,n) i
+        | inRange b i = i - m
+        | True         = error "Ix.Int.index: Index out of range."
+  inRange (m,n) i    = m <= i && i <= n
+
+instance Enum Int8 where
+  toEnum = primToEnumInt8
+  fromEnum = primFromEnumInt8
+#if !defined(TRACING)
+  enumFrom =  numericEnumFrom
+  enumFromThen = numericEnumFromThen
+#else
+  enumFrom n = n : enumFrom (n+1)
+  enumFromThen m n = m : enumFromThen n (2*n-m)
+#endif
+
+instance Read Int8 where
+  readsPrec p = map (\(a,b)->(toEnum a,b)) . readSigned readDec
+
+instance Show Int8 where
+  showsPrec p = showsPrec p . fromEnum
--- src/runtime/Mk/mkCInt.c
+++ src/runtime/Mk/mkCInt.c
@@ -1,5 +1,13 @@
 #include "mk.h"
 
+/* This implementation is believed incorrect.  On some occasions after
+ * a garbage collection, the first byte pointed to by the C-ptr is
+ * mysteriously incremented.  I distrust the CONSTRC(0,1,1) thing:
+ * it is used only here.  In fact, I deprecate the use of mkCInt()
+ * altogether - it seems that mkInt() is perfectly good for storing
+ * a C-ptr in Haskell-land such that it can be recovered intact.
+ */
+
 NodePtr mkCInt(Int i)
 {
   NodePtr n;
