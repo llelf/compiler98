18024867781df4b91511c6fce852a8f8d4f7105c
malcolm <unknown>
2001-01-30 16:53:47 +0000

    Accept {-# pragmas #-} anywhere (removed in the same way as comments)
    provided we don't recognise the pragma name.  A recognised pragma
    still requires a proper parser.
    
    Also, some tidying of this source file to 80-column format.

--- src/compiler98/LexPre.hs
+++ src/compiler98/LexPre.hs
@@ -38,59 +38,62 @@ iPreLex u file r c ('-':'-':xs)
 		skipline cont [] = cont []
 		skipline cont ('\n':r) = cont r
 		skipline cont (_:r) = skipline cont r
-iPreLex u file r c ('{':'-':'#':xs) = (file,r,c,L_LANNOT) :iPreLex u file r (c+3) xs
-iPreLex u file r c ('#':'-':'}':xs) = (file,r,c,L_RANNOT) :iPreLex u file r (c+3) xs
+iPreLex u file r c ('{':'-':'#':xs) =
+                let pragmaName = head (words xs)
+                in
+                if pragmaName `elem` recognisedPragmas then
+                     (file,r,c,L_LANNOT) :iPreLex u file r (c+3) xs
+                else skipcomment u file 0 r (c+3) xs
+iPreLex u file r c ('#':'-':'}':xs) =
+                (file,r,c,L_RANNOT): iPreLex u file r (c+3) xs
 
-iPreLex u file r c ('{':'-':xs) = skipcomment 0 r (c+2) xs
-	where
-		
-		skipcomment :: Int -> Int -> Int -> String -> [PosTokenPre]
-		skipcomment n r c []	       = iPreLex u file r c []
-		skipcomment n r c ('-':'}':xs) = if n > 0 
-						 then skipcomment (n-1) r (c+2) xs
-						 else iPreLex u file r (c+2) xs
-		skipcomment n r c ('{':'-':xs) = skipcomment (n+1) r   (c+2) xs
-		skipcomment n r c ('\n':xs)    = skipcomment n    (r+1)    1 xs
-		skipcomment n r c ('\t':xs)    = skipcomment n     r (tab c) xs
-		skipcomment n r c (_:xs)       = skipcomment n     r   (c+1) xs
+iPreLex u file r c ('{':'-':xs)     = skipcomment u file 0 r (c+2) xs
 iPreLex u file r c ('(':xs) | isTupleId xs =
    case span (==',') xs of
      (commas,')':xs) -> 
  	case length commas of
-	  0 -> (file,r,c,L_ACONID (t_Tuple 0)) :  iPreLex u file r (c+3) xs -- unit ()
-	  n -> (file,r,c,L_ACONID (t_Tuple (n+1))) :  iPreLex u file r (c+n+2) xs -- (n+1)-tuple 
+	-- unit ()
+	  0 -> (file,r,c,L_ACONID (t_Tuple 0)):  iPreLex u file r (c+3) xs
+	-- (n+1)-tuple 
+	  n -> (file,r,c,L_ACONID (t_Tuple (n+1))): iPreLex u file r (c+n+2) xs
  where
    isTupleId xs =
       case dropWhile (==',') xs of
 	(')':_) -> True
 	_ -> False
 
-iPreLex u file r c ('(':'-':'>':')':xs) = (file,r,c,L_ACONID t_Arrow) :  iPreLex u file r (c+4) xs
-iPreLex u file r c ('(':xs) = (file,r,c,L_LPAR)      :  iPreLex u file r (c+1) xs
-iPreLex u file r c (')':xs) = (file,r,c,L_RPAR)      :  iPreLex u file r (c+1) xs
-iPreLex u file r c (',':xs) = (file,r,c,L_COMMA)     :  iPreLex u file r (c+1) xs
-iPreLex u file r c ('{':xs) = (file,r,c,L_LCURL)     :  iPreLex u file r (c+1) xs
-iPreLex u file r c ('}':xs) = (file,r,c,L_RCURL)     :  iPreLex u file r (c+1) xs
-iPreLex u file r c ('[':']':xs) = (file,r,c,L_ACONID t_List)    :  iPreLex u file r (c+2) xs
-iPreLex u file r c ('[':xs) = (file,r,c,L_LBRACK)    :  iPreLex u file r (c+1) xs
-iPreLex u file r c (']':xs) = (file,r,c,L_RBRACK)    :  iPreLex u file r (c+1) xs
-iPreLex u file r c ('`':xs) = (file,r,c,L_BACKTICK)  :  iPreLex u file r (c+1) xs
-iPreLex u file r c (';':xs) = (file,r,c,L_SEMI)      :  iPreLex u file r (c+1) xs
+iPreLex u file r c ('(':'-':'>':')':xs) = (file,r,c,L_ACONID t_Arrow)
+                                                   : iPreLex u file r (c+4) xs
+iPreLex u file r c ('(':xs) = (file,r,c,L_LPAR)    : iPreLex u file r (c+1) xs
+iPreLex u file r c (')':xs) = (file,r,c,L_RPAR)    : iPreLex u file r (c+1) xs
+iPreLex u file r c (',':xs) = (file,r,c,L_COMMA)   : iPreLex u file r (c+1) xs
+iPreLex u file r c ('{':xs) = (file,r,c,L_LCURL)   : iPreLex u file r (c+1) xs
+iPreLex u file r c ('}':xs) = (file,r,c,L_RCURL)   : iPreLex u file r (c+1) xs
+iPreLex u file r c ('[':']':xs) = (file,r,c,L_ACONID t_List)
+                                                   : iPreLex u file r (c+2) xs
+iPreLex u file r c ('[':xs) = (file,r,c,L_LBRACK)  : iPreLex u file r (c+1) xs
+iPreLex u file r c (']':xs) = (file,r,c,L_RBRACK)  : iPreLex u file r (c+1) xs
+iPreLex u file r c ('`':xs) = (file,r,c,L_BACKTICK): iPreLex u file r (c+1) xs
+iPreLex u file r c (';':xs) = (file,r,c,L_SEMI)    : iPreLex u file r (c+1) xs
 iPreLex u file r c ('#':xs) | c == 1 =
        case span ('\n' /=) xs of
-         (line,xs) -> case words line of
-	                (line:file:_) | all isDigit line -> case lexInteger 10 0 line of
-			                                      (_,r,_) -> iPreLex u (packString file) (fromInteger r) 1 (tail xs)
-			_             -> error ("Unknown preprocessor directive at line " ++ show r
-                                               ++ (case show file of "\"\"" -> []; file -> " in file " ++ file) ++ "\n" ++ line ++ "\n")
-iPreLex u file r c ('"':xs) = (file,r,c,L_STRING st) :  iPreLex u file r' c' xs'
-	where
-		(r',c',st,xs') = lexStr r (c+1) xs
-iPreLex u file r c ('\'':xs)= (file,r,c,L_CHAR ch)    :  iPreLex u file r' c' xs'
-	where
-		(r',c',ch,xs') = lexChr r (c+1) xs
+         (line,xs) ->
+            case words line of
+              (line:file:_) | all isDigit line ->
+                case lexInteger 10 0 line of
+                  (_,r,_) ->
+                      iPreLex u (packString file) (fromInteger r) 1 (tail xs)
+              _ -> error ("Unknown preprocessor directive at line " ++ show r
+                         ++ ( case show file of
+                                "\"\"" -> [];
+                                file   -> " in file " ++ file )
+                         ++ "\n" ++ line ++ "\n")
+iPreLex u file r c ('"':xs) = (file,r,c,L_STRING st): iPreLex u file r' c' xs'
+	where (r',c',st,xs') = lexStr r (c+1) xs
+iPreLex u file r c ('\'':xs)= (file,r,c,L_CHAR ch)  : iPreLex u file r' c' xs'
+	where (r',c',ch,xs') = lexChr r (c+1) xs
 
-iPreLex u file r c ('_':[]) = (file,r,c,L_Underscore) :  iPreLex u file r (c+1) []
+iPreLex u file r c ('_':[]) = (file,r,c,L_Underscore): iPreLex u file r (c+1) []
 iPreLex u file r c xxs@('_':xs@(x:_)) =
 	if isAlpha x 
 	then case lexId u r c xxs of
@@ -106,3 +109,25 @@ iPreLex u file r c (xs@(x:s))=
 	        (r,c',lex,xs) -> (file,r,c,lex) : iPreLex u file r c' xs
         else
           (file,r,c,L_ERROR x) :  iPreLex u file r c s
+
+
+-- Extend this list as more pragmas become useful
+recognisedPragmas =
+    "NEED":		-- NEED is found in interface files
+    "#-}":		-- an empty pragma is acceptable
+    map show [0..30]	-- arities are recorded in .hi files as pragmas
+
+
+-- Auxiliary used by more than one clause of iPreLex (originally a local defn)
+skipcomment :: Bool -> PackedString ->Int ->Int ->Int ->String ->[PosTokenPre]
+skipcomment u file n r c xs = skip n r c xs
+  where
+    skip :: Int -> Int -> Int -> String -> [PosTokenPre]
+    skip n r c []           = iPreLex u file r c []
+    skip n r c ('-':'}':xs) = if n > 0 
+				 then skip (n-1) r (c+2) xs
+				 else iPreLex u file r (c+2) xs
+    skip n r c ('{':'-':xs) = skip (n+1) r   (c+2) xs
+    skip n r c ('\n':xs)    = skip n    (r+1)    1 xs
+    skip n r c ('\t':xs)    = skip n     r (tab c) xs
+    skip n r c (_:xs)       = skip n     r   (c+1) xs
