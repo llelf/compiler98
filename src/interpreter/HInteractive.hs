7808c22121604557cad184f0df8b0cb238e1cb96
malcolm <unknown>
2001-02-13 12:33:47 +0000

    Fix string-gaps for newer hbc.
    Still can't fix a bad interaction between cpp and an older hbc.

--- src/interpreter/HInteractive.hs
+++ src/interpreter/HInteractive.hs
@@ -317,42 +317,42 @@ makeclean ext modules = mapM_ (clean ext) modules
 --  if prefix `isPrefixOf` opt then Just (drop (length prefix) opt) else Nothing
 
 
-#if defined(__HBC__)
+#if defined(__HBC__) && !defined(__HASKELL98__)
 banner = "hi - hmake interactive                (Version: "++hmakeVersion++")"
-help   = "hi - help command does not work in hbc"
+help   = "hi - help command does not work in hbc < 0.9999.5"
 
 #else
 banner = "\ 
-\__   __                 __             _____________________________________
-||   ||  ______    ___  || _  ____     hmake interactive (hi):
-||___|| || || ||  ___|| ||/  ||__||       Copyright (c) May 2000
-||---|| || || || ||__|| ||\\_ ||__         http://www.cs.york.ac.uk/fp/hmake/
-||   ||                                Report bugs to: malcolm@cs.york.ac.uk
-||   || Version: "++hmakeVersion++"    -------------------------------------"
+\__   __                 __             _____________________________________\n\ 
+\||   ||  ______    ___  || _  ____     hmake interactive (hi):\n\ 
+\||___|| || || ||  ___|| ||/  ||__||      Copyright (c) May 2000\n\ 
+\||---|| || || || ||__|| ||\\_ ||__        http://www.cs.york.ac.uk/fp/hmake/\n\ 
+\||   ||                                Report bugs to: malcolm@cs.york.ac.uk\n\ 
+\||   || Version: "++hmakeVersion++"    -------------------------------------"
 
 
 help = "\ 
-\Commands (can be abbreviated to first letter):
-  <expr>		evaluate expression
-  :type <expr>	 	show type of expression [nhc98 only]
-  :quit			quit
-  :Quit			quit
-  :load mod [mod...]	load modules (note, not filenames)
-  :load 		clear all modules
-  :also mod [mod...]	load additional modules (note, not filenames)
-  :reload 		repeat last load command
-  :freshen 		remove, recompile, and reload all current modules
-  :module mod		set module scope for evaluating expressions
-  :edit file	 	edit filename
-  :edit 	 	edit current module
-  :cd dir		change directory
-  :cd 			show current directory
-  :dir 			list current directory
-  :hc compiler		set Haskell compiler to use
-  :set options		set hmake/compiler options
-  :unset options	remove hmake/compiler options
-  :observe name		debug function 'name' with 'Hood' [coming soon]
-  :trace [on|off]	switch on/off debugging with 'Hat' [nhc98 only]
-  :!command 		shell escape
-  :?	 		display this list of commands"
+\Commands (can be abbreviated to first letter):\n\ 
+\  <expr>		evaluate expression\n\ 
+\  :type <expr>	 	show type of expression [nhc98 only]\n\ 
+\  :quit			quit\n\ 
+\  :Quit			quit\n\ 
+\  :load mod [mod...]	load modules (note, not filenames)\n\ 
+\  :load 		clear all modules\n\ 
+\  :also mod [mod...]	load additional modules (note, not filenames)\n\ 
+\  :reload 		repeat last load command\n\ 
+\  :freshen 		remove, recompile, and reload all current modules\n\ 
+\  :module mod		set module scope for evaluating expressions\n\ 
+\  :edit file	 	edit filename\n\ 
+\  :edit 	 	edit current module\n\ 
+\  :cd dir		change directory\n\ 
+\  :cd 			show current directory\n\ 
+\  :dir 			list current directory\n\ 
+\  :hc compiler		set Haskell compiler to use\n\ 
+\  :set options		set hmake/compiler options\n\ 
+\  :unset options	remove hmake/compiler options\n\ 
+\  :observe name		debug function 'name' with 'Hood' [coming soon]\n\ 
+\  :trace [on|off]	switch on/off debugging with 'Hat' [nhc98 only]\n\ 
+\  :!command 		shell escape\n\ 
+\  :?	 		display this list of commands"
 #endif
