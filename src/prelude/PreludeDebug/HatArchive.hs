539d4782f2867a1f8ffb265bb2c065ac143b1ff0
malcolm <unknown>
2001-02-23 19:03:33 +0000

    Hat: Need mkTAp[6..12] and to re-export NmType and SR.

--- src/prelude/PreludeDebug/HatArchive.hs
+++ src/prelude/PreludeDebug/HatArchive.hs
@@ -1,4 +1,8 @@
-module HatArchive where
+module HatArchive 
+  ( NmType
+  , SR
+  , module HatArchive
+  ) where
 
 import PackedString (PackedString)
 import HatBuiltin (CStructure,NmType,SR,FileTrace)
@@ -15,6 +19,9 @@ data Trace     = Trace  { traceptr    :: FileTrace
                         , trustedFun  :: Bool
                         , hidden      :: Bool
                         }
+
+sameAs :: Trace -> Trace -> Bool
+sameAs t1 t2 = primSameTrace (traceptr t1) (traceptr t2)
 
 ----
 -- Trace constructors
@@ -61,6 +68,105 @@ mkTAp5 :: Trace		-- trace of application
 	-> Trace	-- arg 5
 	-> SR		-- src ref
 	-> Trace	-- result
+
+mkTAp6 :: Trace		-- trace of application
+	-> Trace	-- fn
+	-> Trace	-- arg 1
+	-> Trace	-- arg 2
+	-> Trace	-- arg 3
+	-> Trace	-- arg 4
+	-> Trace	-- arg 5
+	-> Trace	-- arg 6
+	-> SR		-- src ref
+	-> Trace	-- result
+
+mkTAp7 :: Trace		-- trace of application
+	-> Trace	-- fn
+	-> Trace	-- arg 1
+	-> Trace	-- arg 2
+	-> Trace	-- arg 3
+	-> Trace	-- arg 4
+	-> Trace	-- arg 5
+	-> Trace	-- arg 6
+	-> Trace	-- arg 7
+	-> SR		-- src ref
+	-> Trace	-- result
+
+mkTAp8 :: Trace		-- trace of application
+	-> Trace	-- fn
+	-> Trace	-- arg 1
+	-> Trace	-- arg 2
+	-> Trace	-- arg 3
+	-> Trace	-- arg 4
+	-> Trace	-- arg 5
+	-> Trace	-- arg 6
+	-> Trace	-- arg 7
+	-> Trace	-- arg 8
+	-> SR		-- src ref
+	-> Trace	-- result
+
+mkTAp9 :: Trace		-- trace of application
+	-> Trace	-- fn
+	-> Trace	-- arg 1
+	-> Trace	-- arg 2
+	-> Trace	-- arg 3
+	-> Trace	-- arg 4
+	-> Trace	-- arg 5
+	-> Trace	-- arg 6
+	-> Trace	-- arg 7
+	-> Trace	-- arg 8
+	-> Trace	-- arg 9
+	-> SR		-- src ref
+	-> Trace	-- result
+
+mkTAp10 :: Trace		-- trace of application
+	-> Trace	-- fn
+	-> Trace	-- arg 1
+	-> Trace	-- arg 2
+	-> Trace	-- arg 3
+	-> Trace	-- arg 4
+	-> Trace	-- arg 5
+	-> Trace	-- arg 6
+	-> Trace	-- arg 7
+	-> Trace	-- arg 8
+	-> Trace	-- arg 9
+	-> Trace	-- arg 10
+	-> SR		-- src ref
+	-> Trace	-- result
+
+mkTAp11 :: Trace		-- trace of application
+	-> Trace	-- fn
+	-> Trace	-- arg 1
+	-> Trace	-- arg 2
+	-> Trace	-- arg 3
+	-> Trace	-- arg 4
+	-> Trace	-- arg 5
+	-> Trace	-- arg 6
+	-> Trace	-- arg 7
+	-> Trace	-- arg 8
+	-> Trace	-- arg 9
+	-> Trace	-- arg 10
+	-> Trace	-- arg 11
+	-> SR		-- src ref
+	-> Trace	-- result
+
+mkTAp12 :: Trace		-- trace of application
+	-> Trace	-- fn
+	-> Trace	-- arg 1
+	-> Trace	-- arg 2
+	-> Trace	-- arg 3
+	-> Trace	-- arg 4
+	-> Trace	-- arg 5
+	-> Trace	-- arg 6
+	-> Trace	-- arg 7
+	-> Trace	-- arg 8
+	-> Trace	-- arg 9
+	-> Trace	-- arg 10
+	-> Trace	-- arg 11
+	-> Trace	-- arg 12
+	-> SR		-- src ref
+	-> Trace	-- result
+
 
 mkTNm :: Trace		-- trace of Nm
 	-> NmType	-- NmType
@@ -146,6 +252,7 @@ mkTAp4 tap tfn targ1 targ2 targ3 targ4 sr =
                     , trustedFun = trustedFun tfn
                     , hidden     = False
                     }
+
 mkTAp5 tap tfn targ1 targ2 targ3 targ4 targ5 sr =
     let t = primTAp5 (traceptr tap)
                      (traceptr tfn)
@@ -160,6 +267,143 @@ mkTAp5 tap tfn targ1 targ2 targ3 targ4 targ5 sr =
                     , trustedFun = trustedFun tfn
                     , hidden     = False
                     }
+
+mkTAp6 tap tfn targ1 targ2 targ3 targ4 targ5 targ6 sr =
+    let t = primTAp6 (traceptr tap)
+                     (traceptr tfn)
+                     (traceptr targ1)
+                     (traceptr targ2)
+                     (traceptr targ3)
+                     (traceptr targ4)
+                     (traceptr targ5)
+                     (traceptr targ6)
+                     sr
+    in
+    t `myseq` Trace { traceptr   = t
+                    , trustedFun = trustedFun tfn
+                    , hidden     = False
+                    }
+
+mkTAp7 tap tfn targ1 targ2 targ3 targ4 targ5 targ6 targ7 sr =
+    let t = primTAp7 (traceptr tap)
+                     (traceptr tfn)
+                     (traceptr targ1)
+                     (traceptr targ2)
+                     (traceptr targ3)
+                     (traceptr targ4)
+                     (traceptr targ5)
+                     (traceptr targ6)
+                     (traceptr targ7)
+                     sr
+    in
+    t `myseq` Trace { traceptr   = t
+                    , trustedFun = trustedFun tfn
+                    , hidden     = False
+                    }
+
+mkTAp8 tap tfn targ1 targ2 targ3 targ4 targ5 targ6 targ7 targ8 sr =
+    let t = primTAp8 (traceptr tap)
+                     (traceptr tfn)
+                     (traceptr targ1)
+                     (traceptr targ2)
+                     (traceptr targ3)
+                     (traceptr targ4)
+                     (traceptr targ5)
+                     (traceptr targ6)
+                     (traceptr targ7)
+                     (traceptr targ8)
+                     sr
+    in
+    t `myseq` Trace { traceptr   = t
+                    , trustedFun = trustedFun tfn
+                    , hidden     = False
+                    }
+
+mkTAp9 tap tfn targ1 targ2 targ3 targ4 targ5 targ6 targ7 targ8 targ9 sr =
+    let t = primTAp9 (traceptr tap)
+                     (traceptr tfn)
+                     (traceptr targ1)
+                     (traceptr targ2)
+                     (traceptr targ3)
+                     (traceptr targ4)
+                     (traceptr targ5)
+                     (traceptr targ6)
+                     (traceptr targ7)
+                     (traceptr targ8)
+                     (traceptr targ9)
+                     sr
+    in
+    t `myseq` Trace { traceptr   = t
+                    , trustedFun = trustedFun tfn
+                    , hidden     = False
+                    }
+
+mkTAp10 tap tfn targ1 targ2 targ3 targ4 targ5 targ6 targ7 targ8 targ9 targ10
+        sr =
+    let t = primTAp10 (traceptr tap)
+                     (traceptr tfn)
+                     (traceptr targ1)
+                     (traceptr targ2)
+                     (traceptr targ3)
+                     (traceptr targ4)
+                     (traceptr targ5)
+                     (traceptr targ6)
+                     (traceptr targ7)
+                     (traceptr targ8)
+                     (traceptr targ9)
+                     (traceptr targ10)
+                     sr
+    in
+    t `myseq` Trace { traceptr   = t
+                    , trustedFun = trustedFun tfn
+                    , hidden     = False
+                    }
+
+mkTAp11 tap tfn targ1 targ2 targ3 targ4 targ5 targ6 targ7 targ8 targ9 targ10
+        targ11 sr =
+    let t = primTAp11 (traceptr tap)
+                     (traceptr tfn)
+                     (traceptr targ1)
+                     (traceptr targ2)
+                     (traceptr targ3)
+                     (traceptr targ4)
+                     (traceptr targ5)
+                     (traceptr targ6)
+                     (traceptr targ7)
+                     (traceptr targ8)
+                     (traceptr targ9)
+                     (traceptr targ10)
+                     (traceptr targ11)
+                     sr
+    in
+    t `myseq` Trace { traceptr   = t
+                    , trustedFun = trustedFun tfn
+                    , hidden     = False
+                    }
+
+mkTAp12 tap tfn targ1 targ2 targ3 targ4 targ5 targ6 targ7 targ8 targ9 targ10
+        targ11 targ12 sr =
+    let t = primTAp12 (traceptr tap)
+                     (traceptr tfn)
+                     (traceptr targ1)
+                     (traceptr targ2)
+                     (traceptr targ3)
+                     (traceptr targ4)
+                     (traceptr targ5)
+                     (traceptr targ6)
+                     (traceptr targ7)
+                     (traceptr targ8)
+                     (traceptr targ9)
+                     (traceptr targ10)
+                     (traceptr targ11)
+                     (traceptr targ12)
+                     sr
+    in
+    t `myseq` Trace { traceptr   = t
+                    , trustedFun = trustedFun tfn
+                    , hidden     = False
+                    }
+
 mkTNm tnm nm sr =
     let t = primTNm (traceptr tnm)
                     nm
@@ -273,6 +517,8 @@ mkSR		= primSR3
 ----
 -- Primitive stuff
 ----
+foreign import primSameTrace :: FileTrace -> FileTrace -> Bool
+
 foreign import primTRoot :: FileTrace
 
 foreign import primTAp1 :: FileTrace		-- application
@@ -312,6 +558,104 @@ foreign import primTAp5 :: FileTrace		-- application
 			-> FileTrace		-- arg3
 			-> FileTrace		-- arg4
 			-> FileTrace		-- arg5
+			-> SR			-- src ref
+			-> FileTrace		-- result
+
+foreign import primTAp6 :: FileTrace		-- application
+			-> FileTrace		-- fn
+			-> FileTrace		-- arg1
+			-> FileTrace		-- arg2
+			-> FileTrace		-- arg3
+			-> FileTrace		-- arg4
+			-> FileTrace		-- arg5
+			-> FileTrace		-- arg6
+			-> SR			-- src ref
+			-> FileTrace		-- result
+
+foreign import primTAp7 :: FileTrace		-- application
+			-> FileTrace		-- fn
+			-> FileTrace		-- arg1
+			-> FileTrace		-- arg2
+			-> FileTrace		-- arg3
+			-> FileTrace		-- arg4
+			-> FileTrace		-- arg5
+			-> FileTrace		-- arg6
+			-> FileTrace		-- arg7
+			-> SR			-- src ref
+			-> FileTrace		-- result
+
+foreign import primTAp8 :: FileTrace		-- application
+			-> FileTrace		-- fn
+			-> FileTrace		-- arg1
+			-> FileTrace		-- arg2
+			-> FileTrace		-- arg3
+			-> FileTrace		-- arg4
+			-> FileTrace		-- arg5
+			-> FileTrace		-- arg6
+			-> FileTrace		-- arg7
+			-> FileTrace		-- arg8
+			-> SR			-- src ref
+			-> FileTrace		-- result
+
+foreign import primTAp9 :: FileTrace		-- application
+			-> FileTrace		-- fn
+			-> FileTrace		-- arg1
+			-> FileTrace		-- arg2
+			-> FileTrace		-- arg3
+			-> FileTrace		-- arg4
+			-> FileTrace		-- arg5
+			-> FileTrace		-- arg6
+			-> FileTrace		-- arg7
+			-> FileTrace		-- arg8
+			-> FileTrace		-- arg9
+			-> SR			-- src ref
+			-> FileTrace		-- result
+
+foreign import primTAp10 :: FileTrace		-- application
+			-> FileTrace		-- fn
+			-> FileTrace		-- arg1
+			-> FileTrace		-- arg2
+			-> FileTrace		-- arg3
+			-> FileTrace		-- arg4
+			-> FileTrace		-- arg5
+			-> FileTrace		-- arg6
+			-> FileTrace		-- arg7
+			-> FileTrace		-- arg8
+			-> FileTrace		-- arg9
+			-> FileTrace		-- arg10
+			-> SR			-- src ref
+			-> FileTrace		-- result
+
+foreign import primTAp11 :: FileTrace		-- application
+			-> FileTrace		-- fn
+			-> FileTrace		-- arg1
+			-> FileTrace		-- arg2
+			-> FileTrace		-- arg3
+			-> FileTrace		-- arg4
+			-> FileTrace		-- arg5
+			-> FileTrace		-- arg6
+			-> FileTrace		-- arg7
+			-> FileTrace		-- arg8
+			-> FileTrace		-- arg9
+			-> FileTrace		-- arg10
+			-> FileTrace		-- arg11
+			-> SR			-- src ref
+			-> FileTrace		-- result
+
+foreign import primTAp12 :: FileTrace		-- application
+			-> FileTrace		-- fn
+			-> FileTrace		-- arg1
+			-> FileTrace		-- arg2
+			-> FileTrace		-- arg3
+			-> FileTrace		-- arg4
+			-> FileTrace		-- arg5
+			-> FileTrace		-- arg6
+			-> FileTrace		-- arg7
+			-> FileTrace		-- arg8
+			-> FileTrace		-- arg9
+			-> FileTrace		-- arg10
+			-> FileTrace		-- arg11
+			-> FileTrace		-- arg12
 			-> SR			-- src ref
 			-> FileTrace		-- result
 
