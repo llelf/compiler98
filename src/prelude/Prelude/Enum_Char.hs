395ba8c4c255aa7810d4926f6e93092025ca73de
olaf <unknown>
2001-03-23 18:02:00 +0000

    Fixed definition of instance Enum for tracing.
    For tracing the instance lacked definitions for toEnum and fromEnum.
    The same definitions as for untraced programs can be used.
    Hopefully not more instance definitions are lacking for tracing;
    here it really hurts that the compiler, following the Haskell standard,
    cannot detect missing instance definitions.
    
    Unfortunately a missing instance definition leads to faulty byte code
    for tracing in the current compiler. Still have to modify transformation
    to give proper runtime error.

--- src/prelude/Prelude/Enum_Char.hs
+++ src/prelude/Prelude/Enum_Char.hs
@@ -1,12 +1,13 @@
 module Prelude(Enum(..)) where
 
 instance Enum Char where
-#if !defined(TRACING)
-    toEnum   c = toEnum c	-- MAGIC
-    fromEnum c = fromEnum c	-- MAGIC
 
     enumFrom n        = enumFromTo n (toEnum 255)
     enumFromThen n n' = enumFromThenTo n n' (toEnum 255)
+
+#if !defined(TRACING)
+    toEnum   c = toEnum c	-- MAGIC
+    fromEnum c = fromEnum c	-- MAGIC
 #else
     toEnum   c = _prim _tprim_CharToEnum c
     fromEnum c = _prim _tprim_CharFromEnum c
