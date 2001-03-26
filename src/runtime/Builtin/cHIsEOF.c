2c9a0d5e691c8155fa1512f8371ac224d637dfd2
malcolm <unknown>
2001-03-26 10:54:22 +0000

    hIsEOF does not have the same semantics as feof() in C.
      It appears that feof() does not truly test for end-of-file.
      The EOF marker must be set by a preceding read operation.

--- src/runtime/Builtin/cHIsEOF.c
+++ src/runtime/Builtin/cHIsEOF.c
@@ -35,5 +35,9 @@ C_HEADER(cHIsEOF)
 /* foreign import hIsEOF :: Handle -> IO Bool */
 int hIsEOF (FileDesc* f)
 {
-  return feof(f->fp);
+  int err, c;
+  c = fgetc(f->fp);	/* Horrible but true: feof does not truly test for */
+  err = feof(f->fp);	/* end of file!  The EOF flag must be set by a     */
+  ungetc(c,f->fp);	/* read operation, hence the getc -> ungetc pair.  */
+  return err;
 }
