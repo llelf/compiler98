6ed61f7c5a58a4b2ee7d936e8de3e55a9785739d
malcolm <unknown>
2001-04-24 10:00:33 +0000

    Recent change to Haskell'98 specification of 'recip'.

--- src/prelude/Ratio/Fractional_Ratio.hs
+++ src/prelude/Ratio/Fractional_Ratio.hs
@@ -7,6 +7,6 @@ import Num_Ratio
 
 instance  (Integral a)	=> Fractional (Ratio a)  where
     (x:%y) / (x':%y')	=  (x*y') % (y*x')
-    recip (x:%y)	=  if x < 0 then negate y :% negate x else y :% x
+    recip (x:%y)	=  y%x  --if x < 0 then negate y :% negate x else y:%x
     fromRational (x:%y) =  fromInteger x :% fromInteger y
 
