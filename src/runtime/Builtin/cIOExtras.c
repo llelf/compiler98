1918fc69958d9f37b42043ff3d63d0fc0b918015
malcolm <unknown>
2001-05-23 16:15:04 +0000

    Missing #include <errno.h> for Cygwin

--- src/runtime/Builtin/cIOExtras.c
+++ src/runtime/Builtin/cIOExtras.c
@@ -1,6 +1,7 @@
 /* basic unsafe utilities, defined in IOExtras */
 #include "cinterface.h"
 #include "mk.h"
+#include <errno.h>
 
 void   performGC         ()                     { C_GC(0); }
 int    unsafePtrEq       (void* a, void* b)     { return (a==b); }
