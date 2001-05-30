68da2432b9b8ef3305ef2e5deefdb312808179b5
malcolm <unknown>
2001-05-30 16:07:07 +0000

    Revised H'98: revision was type-incorrect - fix it.

--- src/prelude/PreludeText/Read_Int.hs
+++ src/prelude/PreludeText/Read_Int.hs
@@ -3,6 +3,4 @@ module Prelude(Read(..)) where
 --import Numeric(readSigned,readDec)
 
 instance Read Int where
-  readsPrec p r = (fromInteger i, t)
-                          where
-                            (i,t) = readsPrec p r
+  readsPrec p r = map (\(i,t)-> (fromInteger i, t)) (readsPrec p r)
