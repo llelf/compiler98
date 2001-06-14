521e480eff51a0bdaa7e172488a01c4e13fc7f54
malcolm <unknown>
2001-06-14 12:42:19 +0000

    Hat: Simplify `startDbg' function in Haskell.
    (It no longer needs to connect to the browser when the program is finished.)

--- src/prelude/Prelude/StartDbg.hs
+++ src/prelude/Prelude/StartDbg.hs
@@ -4,9 +4,9 @@ import _Driver
 import DIO
 --import DPrelude(Trace, R(..), myseq)
 
-cConnectToServer primitive 1 :: Trace -> ()
+--cConnectToServer primitive 1 :: Trace -> ()
 
 _startDbg _ =
    case _driver () of
-       R v t -> myseq v (cConnectToServer t)
+       R v t -> myseq v () 	-- (cConnectToServer t)
 
