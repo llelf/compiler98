2e45fe9593c222c63db5960349878bcf9b99b567
malcolm <unknown>
2001-07-03 16:45:50 +0000

    Add  fixPriority :: TraceId -> Int  (uses current byte encoding).

--- src/compiler98/TraceId.hs
+++ src/compiler98/TraceId.hs
@@ -7,6 +7,7 @@ module TraceId
   , tokenId		-- :: TraceId -> TokenId
   , arity		-- :: TraceId -> Maybe Int
   , isLambdaBound	-- :: TraceId -> Bool
+  , fixPriority		-- :: TraceId -> Int
   ) where
 
 import TokenId (TokenId)
@@ -42,3 +43,13 @@ arity (_,Just aux) = let a = args aux in if a==(-1) then Nothing else Just a
 isLambdaBound :: TraceId -> Bool
 isLambdaBound (_,Nothing)  = error "TraceId.isLambdaBound: no aux information"
 isLambdaBound (_,Just aux) = not (letBound aux)
+
+fixPriority :: TraceId -> Int
+fixPriority (_,Nothing) = 3	-- default fixity and priority
+fixPriority (_,Just info) = encode (fixity info) (priority info)
+  where
+    encode Def     _ = 3
+    encode L       n = 2 + (n*4)
+    encode R       n = 1 + (n*4)
+    encode None    n = 0 + (n*4)
+    encode (Pre _) n = 0 + (n*4)
