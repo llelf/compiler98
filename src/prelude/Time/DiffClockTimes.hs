f4076eeee3985be0cf61e424e485d9ea16d6b4e1
malcolm <unknown>
2001-10-26 15:21:31 +0000

    Typos.

--- src/prelude/Time/DiffClockTimes.hs
+++ src/prelude/Time/DiffClockTimes.hs
@@ -5,7 +5,7 @@ import DTimeDiff
 
 diffClockTimes :: ClockTime -> ClockTime -> TimeDiff
 diffClockTimes (CT t) (CT t') =
-    TimeDiff { tdYear=0, tdMon=0, tdDay=dd
+    TimeDiff { tdYear=0, tdMonth=0, tdDay=dd
              , tdHour=dh, tdMin=dm, tdSec=ds, tdPicosec=0 }
   where
     ts = t-t'
