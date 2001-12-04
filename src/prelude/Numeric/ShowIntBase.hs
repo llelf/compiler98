ec5019a3c98883efef7b849b34d03f569d51d5e4
malcolm <unknown>
2001-12-04 13:20:08 +0000

    Track recent changes to Haskell'98.  The Numeric library gains
    the functions showIntAtBase, showHex, showOct.

--- src/prelude/Numeric/ShowIntBase.hs
+++ src/prelude/Numeric/ShowIntBase.hs
@@ -2,10 +2,15 @@ module Numeric where
 
 import Char (intToDigit)
 
-showIntBase :: Integral a => a -> a -> ShowS
-showIntBase base n r
-  | n < 0   = error "Numeric.showIntBase: can't show negative numbers"
+showIntAtBase :: Integral a => a -> (Int->Char) -> a -> ShowS
+showIntAtBase base intToDig n r
+  | n < 0   = error "Numeric.showIntAtBase: can't show negative numbers"
   | otherwise =
       let (n',d) = quotRem n base
-          r'     = intToDigit (fromIntegral d) : r
-      in if n' == 0 then r' else showIntBase base n' r'
+          r'     = intToDig (fromIntegral d) : r
+      in if n' == 0 then r' else showIntAtBase base intToDig n' r'
+
+showHex, showOct, showBin :: Integral a => a -> ShowS
+showHex = showIntAtBase 16 intToDigit
+showOct = showIntAtBase 8 intToDigit
+showBin = showIntAtBase 2 intToDigit
--- src/prelude/Numeric/ShowSigned.hs
+++ src/prelude/Numeric/ShowSigned.hs
@@ -1,6 +1,6 @@
 module Numeric where
 
-showSigned:: (Num a, Ord a) => (a -> ShowS) -> Int -> a -> ShowS
+showSigned:: (Real a) => (a -> ShowS) -> Int -> a -> ShowS
 showSigned showPos p x = 
 	 if x < 0 then showParen (p > 6)
 	     (showChar '-' . showPos (negate x))
