ce2099ea1f9e71991dce65272676223286d02156
malcolm <unknown>
2002-02-05 16:16:51 +0000

    Hack to avoid compiler error with -T.

--- src/prelude/IOExtras/DIORef.hs
+++ src/prelude/IOExtras/DIORef.hs
@@ -4,6 +4,7 @@ module IOExtras
   ) where
 
 import FFI
-import PreludeBuiltin(Vector)
+import PreludeBuiltin (Vector)
+import IO (Handle)	-- hack to avoid compiler error with -T
 
 newtype IORef a = IORef (Vector a)	-- vector contains only one element!
