29b2bcd011264bd330168d0357b175ad6fff7750
malcolm <unknown>
2002-02-28 15:55:13 +0000

    Follow the revised Haskell'98 behaviour of 'take' and 'drop' in
    the presence of a negative Int argument.

--- src/prelude/PreludeList/Drop.hs
+++ src/prelude/PreludeList/Drop.hs
@@ -1,26 +1,7 @@
 module Prelude where
 
 drop			:: Int -> [a] -> [a]
+drop n xs | n<=0         = xs
+drop _ []                = []
+drop n (x:xs)            = drop (n-1) xs
 
---drop 0 xs	      = xs
---drop _ []	      = []
---drop n (x:xs) | n>0 = drop (n-1) xs
---drop _ _          = error "PreludeList.drop: negative argument"
-
-#if 1
-drop n xs 
-    | n<0  = error "PreludeList.drop: negative argument"  -- xs
-    | n>=0 = drop' n xs
-  where drop' 0 xs     = xs
-        drop' n []     = []
-        drop' n (x:xs) = drop' (n-1) xs
-
-#else
-drop n xs 
-    | n<0  = error "PreludeList.drop: negative argument"  -- xs
-    | n>=0 = drop' n xs
-  where drop' n xs | n==0  = xs
-        drop' n []         = []
-        drop' n (x:xs)     = drop' (n-1) xs
-
-#endif
--- src/prelude/PreludeList/Take.hs
+++ src/prelude/PreludeList/Take.hs
@@ -1,25 +1,7 @@
 module Prelude where
 
 take		      :: Int -> [a] -> [a]
---take 0 _	      = []
---take _ []	      = []
---take n (x:xs) | n>0 = x : take (n-1) xs
---take _ _            = error "PreludeList.take: negative argument"
+take n _ | n<=0        = []
+take _ []              = []
+take n (x:xs)          = x : take (n-1) xs
 
-#if 1
-take n xs
-    | n<0  = error "PreludeList.take: negative argument"  -- []
-    | n>=0 = take' n xs
-  where take' 0 xs     = []
-        take' n []     = []
-        take' n (x:xs) = x : take' (n-1) xs
-
-#else
-take n xs
-    | n<0  = error "PreludeList.take: negative argument"  -- []
-    | n>=0 = take' n xs
-  where take' n xs | n==0  = []
-        take' n []         = []
-        take' n (x:xs)     = x : take' (n-1) xs
-
-#endif
