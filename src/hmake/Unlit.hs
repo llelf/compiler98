14371f9dca68d28d04e0e4e781aea7a2739519c9
malcolm <unknown>
2002-06-14 10:35:48 +0000

    Some reformatting, and use more efficient version of `lines'.

--- src/hmake/Unlit.hs
+++ src/hmake/Unlit.hs
@@ -1,16 +1,22 @@
+-- Part of the following code is from
+-- "Report on the Programming Language Haskell",
+--   version 1.2, appendix C.
 module Unlit(unlit,plain) where
 
-
---import Prelude((.),not,otherwise,(&&),(||),fromIntegral,($))
---import Prelude(lines,unlines,all,tail,concat,show,error,words,read)
---import PreludeList(map,(++))
 import Char
 
--- Part of the following code is from
--- "Report on the Programming Language Haskell", version 1.2, appendix C.
+-- exports:
+
+unlit :: String -> String -> String
+unlit file lhs = (unlines . map unclassify . adjacent file (0::Int) Blank
+                 . classify 0) (tolines lhs)
+
+plain :: String -> String -> String	-- no unliteration
+plain file hs = hs
+----
 
 data Classified = Program String | Blank | Comment
-		 | Include Int String | Pre String
+		| Include Int String | Pre String
 
 classify :: Int -> [String] -> [Classified]
 classify _ []                = []
@@ -37,13 +43,6 @@ unclassify (Include i f) = '#':' ':show i ++ ' ':f
 unclassify Blank       = ""
 unclassify Comment     = ""
 
-unlit :: String -> String -> String
-unlit file lhs = (unlines . map unclassify . adjacent file (0::Int) Blank
-                 . classify 0) (lines lhs)
-
-plain :: String -> String -> String	-- no unliteration
-plain file hs = hs
-
 adjacent :: String -> Int -> Classified -> [Classified] -> [Classified]
 adjacent file 0 _             (x              :xs) = x: adjacent file 1 x xs
 					-- force evaluation of line number
@@ -66,8 +65,21 @@ message file   n p c = "In file " ++ file ++ " at line "
                               ++show n++": "++p++ " line before "++c++" line.\n"
 
 
+-- Re-implementation of 'lines', for better efficiency (but decreased
+-- laziness).  Also, importantly, accepts non-standard DOS and Mac line
+-- ending characters.
+tolines s = lines' s id
+  where
+  lines' []             acc = [acc []]
+  lines' ('\^M':'\n':s) acc = acc [] : lines' s id      -- DOS
+  lines' ('\^M':s)      acc = acc [] : lines' s id      -- MacOS
+  lines' ('\n':s)       acc = acc [] : lines' s id      -- Unix
+  lines' (c:s)          acc = lines' s (acc . (c:))
+
+
 
 {-
+-- A very naive version of unliteration....
 module Unlit(unlit) where
 -- This version does not handle \begin{code} & \end{code}, and it is
 -- careless with indentation.
