69705a965f396dabeaf23dbc3b8bb2fbab3bc9e0
malcolm <unknown>
2002-06-24 10:48:41 +0000

    Maybe library should re-export appropriate Prelude definitions.

--- src/prelude/Maybe/Maybe.hs
+++ src/prelude/Maybe/Maybe.hs
@@ -1,6 +1,12 @@
-module Maybe(
-    isJust, isNothing, fromJust, fromMaybe, listToMaybe, maybeToList,
-    catMaybes, mapMaybe ) where
+module Maybe
+  ( isJust, isNothing
+  , fromJust, fromMaybe, listToMaybe, maybeToList
+  , catMaybes, mapMaybe
+
+  -- ... and what the Prelude exports
+  , Maybe(Nothing,Just)
+  , maybe
+  ) where
 
 
 import IsJust
