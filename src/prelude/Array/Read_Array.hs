d8c7ff01b711d564ef487d99c61d893ec432fd6e
malcolm <unknown>
2002-08-07 12:39:40 +0000

    Update Read/Show instances of Array in line with latest revision of the Report.

--- src/prelude/Array/Read_Array.hs
+++ src/prelude/Array/Read_Array.hs
@@ -3,9 +3,11 @@ module Array(Read(..)) where
 import Ix
 import DArray
 import ArrayFun
+import ArrPrec
 
 instance  (Ix a, Read a, Read b) => Read (Array a b)  where
-    readsPrec p = readParen (p > 9)
+    readsPrec p = readParen (p > arrPrec)
            (\r -> [(array b as, u) | ("array",s) <- lex r,
-                                     (b,t)       <- reads s,
-                                     (as,u)      <- reads t   ])
+                                     (b,t)       <- readsPrec (arrPrec+1) s,
+                                     (as,u)      <- readsPrec (arrPrec+1) t   ])
+
