e7c64041d4c8dee5fdfd3abbc2e336b36c6abb10
malcolm <unknown>
2002-08-07 12:41:41 +0000

    Update Read/Show instances of Ratio to the latest revision of the Report.

--- src/prelude/Ratio/Read_Ratio.hs
+++ src/prelude/Ratio/Read_Ratio.hs
@@ -6,7 +6,8 @@ import Prec	(prec)
 import RatioCon	((%))
 
 instance  (Read a,Integral a) => Read (Ratio a)  where
-    readsPrec p  =  readParen (p > prec)
-			      (\r -> [(x % y,u) | (x,s)   <- reads r,
-					          ("%",t) <- lex s,
-						  (y,u)   <- reads t ])
+    readsPrec p =
+        readParen (p > prec)
+                  (\r -> [(x % y,u) | (x,s)   <- readsPrec (prec+1) r
+                                    , ("%",t) <- lex s
+                                    ,  (y,u)   <- readsPrec (prec+1) t ])
--- src/prelude/Ratio/Show_Ratio.hs
+++ src/prelude/Ratio/Show_Ratio.hs
@@ -6,7 +6,9 @@ import Prec
 
 instance  (Integral a) => Show (Ratio a)  where
     showsPrec p (x:%y)	=  showParen (p > prec)
-    	    	    	       (shows x . showString " % " . shows y)
+    	    	    	       (showsPrec (prec+1) x
+				 . showString " % "
+				 . showsPrec (prec+1) y)
 
     showsType ~(x:%y) = showString "(Ratio " . showsType x . showChar ')'
 
