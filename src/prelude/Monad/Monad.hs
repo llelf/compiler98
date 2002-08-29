4c1a7cdf4b3cd951c66a64cb2469f42efab7ffb8
malcolm <unknown>
2002-08-29 11:01:47 +0000

    Add a comment.

--- src/prelude/Monad/Monad.hs
+++ src/prelude/Monad/Monad.hs
@@ -4,6 +4,8 @@ module Monad
   , msum
   , filterM, mapAndUnzipM, zipWithM, zipWithM_, foldM
   , liftM, liftM2, liftM3, liftM4, liftM5
+
+  -- ... and what the Prelude exports
   , Monad((>>=), (>>), return, fail)
   , Functor(fmap)
   , mapM, mapM_, sequence, sequence_, (=<<)
