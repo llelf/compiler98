86787c312873ac89514dcca70a68fe6b9afce571
malcolm <unknown>
2003-09-02 15:27:27 +0000

    Correct the semantics of fixIO, as noted by Ross Paterson.

--- src/prelude/IOExtras/FixIO.hs
+++ src/prelude/IOExtras/FixIO.hs
@@ -1,7 +1,10 @@
 module NHC.IOExtras where
 
-import NHC.Internal (unsafePerformIO)
+import NHC.Internal (IO(..))
 
 fixIO :: (a -> IO a) -> IO a
-fixIO f = let x = unsafePerformIO (f x)
-          in return x
+fixIO f = IO (\w -> let { IO f' = f a; r = f' w; Right a = r } in r)
+
+{- The following implementation is incorrect.
+-- fixIO f = let x = unsafePerformIO (f x) in return x
+-}
