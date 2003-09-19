59199cb45c306ec4c4061907eac424578c49ce16
malcolm <unknown>
2003-09-19 09:12:05 +0000

    Protect trailing backslash.

--- src/prelude/List/Difference.hs
+++ src/prelude/List/Difference.hs
@@ -2,7 +2,7 @@ module List where
 
 import Delete
 
-infix 5 \\
+infix 5 \\ 	-- comment is to protect trailing backslash from gcc >= 3.3
 
 -- list difference (non-associative).  In the result of xs \\ ys,
 -- the first occurrence of each element of ys in turn (if any)
