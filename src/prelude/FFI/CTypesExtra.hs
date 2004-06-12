864a07798d3b0a2ebce933642de6ca2cc5037bab
panne <unknown>
2004-06-12 23:24:28 +0000

    * Changed CPtrdiff, CWchar, and CSigAtomic into INTEGRAL_TYPES
    * Same for CSize, CClock, and CTime. Note the type => newtype change here
    * Factored out the macro hacks
    
    Note that this implies some mysterious changes to some include/...hi files, but
    lacking some information I'm totally unable to track the necessary changes here.
    Some help is needed, the best I could manage was:
    
       Cannot find constructor for newtype: InfoData 92 NHC.FFI.CPtrdiff IEnone 92 (DataNewType False [])
    
    This happened after copying the generated FFI.hi plus adding finalizerFree to it
    and compiling some code (= OpenGL package) containing "fromIntegral foo" where
    foo has the type CPtrdiff.

--- src/prelude/FFI/CTypesExtra.hs
+++ src/prelude/FFI/CTypesExtra.hs
@@ -12,21 +12,23 @@ module NHC.FFI
 
   ) where
 
-import Int
-import Word
+import NonStdUnsafeCoerce
+import Storable	( Storable(..) )
+import Monad	( liftM )
+import Ptr	( castPtr )
 import CTypes
+
+#include "CTypes.h"
 
 -- So far, we just make a best guess at these types for most 32-bit machines.
 -- Should really be auto-configured.
 
-newtype CPtrdiff   = CPtrdiff CInt	-- ??
-newtype CWchar     = CWchar CInt	-- ??
-newtype CSigAtomic = CSigAtomic CInt	-- ??
-
--- Should be newtypes perhaps?
-type CSize      = CInt		-- size_t
-type CClock     = CUInt		-- clock_t
-type CTime      = CUInt		-- time_t
+INTEGRAL_TYPE(CPtrdiff,CInt)
+INTEGRAL_TYPE(CWchar,CInt)
+INTEGRAL_TYPE(CSigAtomic,CInt)
+INTEGRAL_TYPE(CSize,CInt)
+INTEGRAL_TYPE(CClock,CUInt)
+INTEGRAL_TYPE(CTime,CUInt)
 
 -- Not sure what these are for??
 data CFile   = CFile		-- ??
