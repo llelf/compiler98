af4866741a8a37773fadf17faa0970182e63ea02
panne <unknown>
2004-06-20 17:36:14 +0000

    * Move the header to include from OPTIONS to the foreign imports
    * Tell GHC that the calls can be handled unsafe
    
    Note: Perhaps some #ifdefs are necessary for ancient GHCs, but it must
    be ensured that the Float is passed correctly to the C side. This
    wasn't the case before.

--- src/compiler98/Floats.hs
+++ src/compiler98/Floats.hs
@@ -1,10 +1,10 @@
-{-# OPTIONS -fglasgow-exts -#include "ghc_floats.h" -fvia-C #-}
+{-# OPTIONS -fglasgow-exts #-}
 module Floats
   ( floatToInt
   , doubleToInt0
   , doubleToInt1
   ) where
 
-foreign import ccall floatToInt   :: Float -> Int
-foreign import ccall doubleToInt0 :: Double -> Int
-foreign import ccall doubleToInt1 :: Double -> Int
+foreign import ccall unsafe "ghc_floats.h floatToInt"   floatToInt   :: Float  -> Int
+foreign import ccall unsafe "ghc_floats.h doubleToInt0" doubleToInt0 :: Double -> Int
+foreign import ccall unsafe "ghc_floats.h doubleToInt1" doubleToInt1 :: Double -> Int
