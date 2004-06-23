102883faee912e55da545e9ba95dcf38ad914906
malcolm <unknown>
2004-06-23 09:11:32 +0000

    wibble

--- src/compiler98/Type.hs
+++ src/compiler98/Type.hs
@@ -613,7 +613,7 @@ typeExp (ExpType pos exp ctxs t) = -- Ignoring ctx and doesn't check if the
                                    -- free variables really are free !!!
   (if not (null ctxs) 
   then strace ("Context at " ++ strPos (getPos ctxs)
-              ++ " in type-annotated expression is ignored :-(")
+              ++ " in type-annotated expression is ignored :-(\n")
   else id) $
   typeExp exp >>>= \ (exp,expT) ->
   let nt = type2NT t
@@ -666,7 +666,7 @@ typePat (ExpType pos exp ctxs t) = -- Ignoring ctx and doesn't check if the
                                    -- free variables really are free !!!
   (if not (null ctxs) 
   then strace ("Context at " ++ strPos (getPos ctxs)
-              ++ " in type-annotated pattern is ignored :-(")
+              ++ " in type-annotated pattern is ignored :-(\n")
   else id) $
   typePat exp >>>= \ (exp,expT,eTVar) ->
   let nt = type2NT t
