18138d81cc8949925e45486d31e31d73d00a6be6
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2007-06-22 13:15:24 +0000

    remove cpp #ifdefs

--- SysDeps.hs
+++ SysDeps.hs
@@ -2,19 +2,8 @@ module SysDeps (
    module PackedString, trace, openBinaryFileWrite, osName
 ) where
 
-#if __GLASGOW_HASKELL__ >= 502
 import Data.PackedString as PackedString
-#else
-import PackedString
-#endif
-
-#if defined(__NHC__) || defined(__HBC__)
-import NonStdTrace (trace)
-#elif __GLASGOW_HASKELL__ >= 502
 import Debug.Trace (trace)
-#else
-import IOExts      (trace)
-#endif
 
 import System.IO
 import System.Info
